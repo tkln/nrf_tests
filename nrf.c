ea377165f705104fdea48928f1a5d2166ca13cac
Aapo Vienamo <aapo.vienamo@iki.fi>
2013-07-10 12:31:19 +0300

    clean up the code

--- nrf.c
+++ nrf.c
@@ -8,12 +8,36 @@ void nrf_init(void)
     spi_master_init();
     /* init the nrf */
     /*PWR_UP bit in CONFIG, standby-1 */
-    NRF_CE_PORT &= ~(1<<NRF_CE);
+    NRF_CE_LO();
     NRF_CE_DDR |= (1<<NRF_CE);
     _delay_ms(11);
     nrf_set_reg_bitmask(REG_CONFIG, (1<<PWR_UP));
     nrf_set_reg(REG_RX_PW_P0, 1);
 }
+
+/* mode setting */
+
+void nrf_tx_mode(void)
+{
+    nrf_set_reg_bitmask(REG_CONFIG, (1<<PWR_UP));
+    nrf_unset_reg_bitmask(REG_CONFIG, (1<<PRIM_RX));
+    NRF_CE_HI();
+}
+
+void nrf_rx_mode(void)
+{
+    nrf_set_reg_bitmask(REG_CONFIG, (1<<PWR_UP) | (1<<PRIM_RX));
+    NRF_CE_HI();
+}
+
+void nrf_standby_mode(void)
+{
+    nrf_set_reg_bitmask(REG_CONFIG, (1<<PWR_UP));
+    nrf_unset_reg_bitmask(REG_CONFIG, (1<<PRIM_RX));
+    NRF_CE_LO();
+}
+
+/* nrf commands over spi */
 
 /* len is the amount of data bytes (excluding the command) */
 uint8_t nrf_command_r(uint8_t command, uint8_t *data, size_t len)
@@ -55,43 +79,29 @@ uint8_t nrf_command_w(uint8_t command, uint8_t *data, size_t len)
     return status;
 }
 
+/* register operations */
+
 uint8_t nrf_set_reg_buf(uint8_t reg, uint8_t *data, size_t len)
 {
     /* register is address is 5 bits */
     return nrf_command_w((0x1f & reg) | (1<<5), data, len);
 }
+
 uint8_t nrf_get_reg_buf(uint8_t reg, uint8_t *data, size_t len)
 {
     return nrf_command_r(R_REGISTER & reg, data, len);
 }
+
 uint8_t nrf_set_reg(uint8_t reg, uint8_t data)
 {
     return nrf_command_w((0x1f & reg) | (1<<5), &data, 1);
 }
+
 uint8_t nrf_get_reg(uint8_t reg, uint8_t *data)
 {
     return nrf_command_r(R_REGISTER & reg, data, 1);
 }
-void nrf_tx_mode(void)
-{
-    nrf_set_reg_bitmask(REG_CONFIG, (1<<PWR_UP));
-    nrf_unset_reg_bitmask(REG_CONFIG, (1<<PRIM_RX));
-    NRF_CE_PORT |= (1<<NRF_CE);
-}
-void nrf_rx_mode(void)
-{
-    nrf_set_reg_bitmask(REG_CONFIG, (1<<PWR_UP) | (1<<PRIM_RX));
-}
-void nrf_fifo_single_tx(void)
-{
-    nrf_set_reg_bitmask(REG_CONFIG, (1<<PWR_UP));
-    nrf_unset_reg_bitmask(REG_CONFIG, (1<<PRIM_RX));
-    _delay_ms(10);
-    NRF_CE_PORT |= (1<<NRF_CE);
-    _delay_us(20);
-    NRF_CE_PORT &= ~(1<<NRF_CE);
-    _delay_ms(10);
-}
+
 uint8_t nrf_set_reg_bitmask(uint8_t reg, uint8_t mask)
 {
     uint8_t reg_val;
@@ -106,5 +116,24 @@ uint8_t nrf_unset_reg_bitmask(uint8_t reg, uint8_t mask)
     nrf_get_reg(reg, &reg_val);
     reg_val &= ~(mask);
     return nrf_set_reg(reg, reg_val);
+}
+
+uint8_t nrf_reg_bitmask(uint8_t reg, uint8_t set_mask, uint8_t unset_mask)
+{
+    nrf_set_reg_bitmask(reg, set_mask);
+    return nrf_unset_reg_bitmask(reg, unset_mask);
+}
+
+/* fifo operations */
+
+void nrf_fifo_single_tx(void)
+{
+    nrf_set_reg_bitmask(REG_CONFIG, (1<<PWR_UP));
+    nrf_unset_reg_bitmask(REG_CONFIG, (1<<PRIM_RX));
+    _delay_ms(10);
+    NRF_CE_PORT |= (1<<NRF_CE);
+    _delay_us(20);
+    NRF_CE_PORT &= ~(1<<NRF_CE);
+    _delay_ms(10);
 }
 
--- nrf.h
+++ nrf.h
@@ -115,26 +115,33 @@
 #define NRF_CE_PORT PORTB 
 #define NRF_CE_DDR DDRB
 #define NRF_CE 4
+#define NRF_CE_HI() NRF_CE_PORT |= (1<<NRF_CE)
+#define NRF_CE_LO() NRF_CE_PORT &= ~(1<<NRF_CE)
 
 void nrf_init(void);
-uint8_t nrf_set_reg_buf(uint8_t reg, uint8_t *data, size_t len);
 
+/* mode setting */
+void nrf_tx_mode(void);
+void nrf_rx_mode(void);
+
+/* nrf commands over spi */
+uint8_t nrf_command_r(uint8_t command, uint8_t *data, size_t len);
+uint8_t nrf_command_w(uint8_t command, uint8_t *data, size_t len);
+
+/* register operations */
 /* The datasheet has the max lenghts, use a large enough preallocated buffer 
  * for the return value. The actual lenght is placed to the location len 
  * points to.
  */
+uint8_t nrf_set_reg_buf(uint8_t reg, uint8_t *data, size_t len);
 uint8_t nrf_get_reg_buf(uint8_t reg, uint8_t *data, size_t len);
 uint8_t nrf_set_reg(uint8_t reg, uint8_t data);
 uint8_t nrf_get_reg(uint8_t reg, uint8_t *data);
-/* enable tx mode and empty the fifo */
-uint8_t nrf_tx(void);
 /* write data to tx payload fifo */
-uint8_t nrf_fifo_put(uint8_t *data, size_t len);
-uint8_t nrf_command_r(uint8_t command, uint8_t *data, size_t len);
-uint8_t nrf_command_w(uint8_t command, uint8_t *data, size_t len);
-void nrf_tx_mode(void);
-void nrf_rx_mode(void);
-void nrf_fifo_single_tx(void);
 uint8_t nrf_set_reg_bitmask(uint8_t reg, uint8_t mask);
 uint8_t nrf_unset_reg_bitmask(uint8_t reg, uint8_t mask);
+uint8_t nrf_reg_bitmask(uint8_t reg, uint8_t set_mask, uint8_t unset_mask);
+
+/* fifo operations */
+void nrf_fifo_single_tx(void);
 #endif
